92755e8a70b152e684df88575f8b873b332c4cf2
shyamg91 <shyamg7777@gmail.com>
2017-03-24 16:18:48 +1300

    Click through

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     'angular'
   ],
   rules: {
-    'angular/no-service-method': 0
+    'angular/no-service-method': 0,
+    'brace-style': 0,
+    'angular/document-service': 0
   }
 }
--- src/app/module/module.js
+++ src/app/module/module.js
@@ -12,8 +12,45 @@ function moduleController($http, $log, $stateParams) {
           vm.module = data;
         }
       });
-      $log.debug(vm.module);
     }, function (err) {
       $log.debug(err);
     });
+
+  vm.key = function ($event) {
+    $log.debug($event.keyCode);
+    if ($event.keyCode === 38) {
+      $log.debug('up arrow');
+    }
+    else if ($event.keyCode === 39) {
+      $log.debug('right arrow');
+    }
+    else if ($event.keyCode === 40) {
+      $log.debug('down arrow');
+    }
+    else if ($event.keyCode === 37) {
+      $log.debug('left arrow');
+    }
+    var video = document.getElementById('vid');
+    var frameTime = 5; // assume 25 fps
+    if (video.paused) {
+      // or you can force it to pause here
+      if ($event.keyCode === 37) {
+        // left arrow
+        if (video.currentTime > 0) {
+          // one frame back
+          video.currentTime -= frameTime;
+          $log.debug(video.currentTime);
+        }
+      } else if ($event.keyCode === 39) {
+        // right arrow
+        $log.debug('Condition', video.currentTime < video.duration);
+        if (video.currentTime < video.duration) {
+          // one frame forward
+          // Don't go past the end, otherwise you may get an error
+          video.currentTime = Math.min(video.duration, video.currentTime + frameTime);
+          $log.debug('Current Time : ', video.currentTime, ' Total : ', video.duration);
+        }
+      }
+    }
+  };
 }
